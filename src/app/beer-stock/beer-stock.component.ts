17acba784797002cb49b2d5757ab2a202ae94e0c
Jim Padilla and Margie Nevarez <jim@>
2017-01-18 15:48:44 -0800

    added color coding for price

--- src/app/beer-stock/beer-stock.component.ts
+++ src/app/beer-stock/beer-stock.component.ts
@@ -11,6 +11,17 @@ export class BeerStockComponent {
     new Keg('Ambar', 'Dos Equis', 2.99, 4.7, 1.5),
     new Keg('Epicenter Amber', 'SanTan', 3.49, 5.5, 1),
     new Keg('American Lager', 'Pabst Blue Ribbon', 1.49, 4.7, 4),
-    new Keg('Total Domination', 'Ninkasi', 3.49, 6.7, 3)
+    new Keg('Total Domination', 'Ninkasi', 3.49, 6.7, 3),
+    new Keg('Hoppy Bunny Aba', 'The Duck-Rabbit', 4.79, 7.3, 2)
   ]
+
+  priceColor(currentKeg) {
+    if (currentKeg.price <= 2) {
+      return "bg-primary";
+    } else if (currentKeg.price > 2 && currentKeg.price <= 4) {
+      return "bg-warning";
+    } else if (currentKeg.price > 4) {
+      return "bg-success";
+    }
+  }
 }
