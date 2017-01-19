7bc72fa65274fa165f2757d3a0904577df525799
Jim Padilla and Margie Nevarez <jim+margie@>
2017-01-18 18:13:17 -0800

    Add beer-stock component, keg class

--- src/app/beer-stock/beer-stock.component.ts
+++ src/app/beer-stock/beer-stock.component.ts
@@ -8,11 +8,11 @@ import { Keg } from '../keg';
 
 export class BeerStockComponent {
   kegs : Keg[] = [
-    new Keg('Ambar', 'Dos Equis', 2.99, 4.7, 1.5),
-    new Keg('Epicenter Amber', 'SanTan', 3.49, 5.5, 1),
-    new Keg('American Lager', 'Pabst Blue Ribbon', 1.49, 4.7, 4),
-    new Keg('Total Domination', 'Ninkasi', 3.49, 6.7, 3),
-    new Keg('Hoppy Bunny Aba', 'The Duck-Rabbit', 4.79, 7.3, 2)
+    new Keg('Ambar', 'Dos Equis', 2.99, 4.7, 2, 62),
+    new Keg('Epicenter Amber', 'SanTan', 3.49, 5.5, 1, 124),
+    new Keg('American Lager', 'Pabst Blue Ribbon', 1.49, 4.7, 4, 124),
+    new Keg('Total Domination', 'Ninkasi', 3.49, 6.7, 3, 124),
+    new Keg('Hoppy Bunny Aba', 'The Duck-Rabbit', 4.79, 7.3, 2, 124)
   ]
 
   priceColor(currentKeg) {
@@ -24,4 +24,10 @@ export class BeerStockComponent {
       return "bg-success";
     }
   }
+
+  sellPint(currentKeg, amount) {
+    console.log('clicked');
+    return currentKeg.pints - amount;
+  }
+
 }
